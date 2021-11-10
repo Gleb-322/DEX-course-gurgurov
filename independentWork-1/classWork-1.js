9884f40a62bf5d86261f127073879adb2ff7326a
gleb <gyrra98@mail.ru>
2021-11-10 13:43:37 +0200

    complete last quest

--- independentWork-1/classWork-1.js
+++ independentWork-1/classWork-1.js
@@ -1,37 +1,26 @@
-// export const getHolidayPrizes = (f, s, t) => {
-//     let nowDate = new Date()
-//     let staffDate = new Date(s)
-//     let getDate = s.getDate()
-//     let getMonth = s.getMonth()
-//     let newZp = (10 * t / 100) + (getDate * getMonth)
-//     if (nowDate === staffDate) {
-//       return `У ${f} сегодня праздник, его премия составляет ${newZp}`
-//     }
-//   };
+export const dataProcessingAboutSalary = (f, s) => {
+  return f + s;
+};
 
-  function getHolidayPrizes (f, s, t)  {
-    
-    let nowDate = new Date()
-    let getNowDate = nowDate.getDate()
-    let getNowMonth = nowDate.getMonth()
-    let staffDate = new Date(Date.parse(s))
-    let getStaffDate = staffDate.getDate()
-    let getStaffMonth = staffDate.getMonth()
-    
-    let newZp = (10 * t / 100) + (getStaffDate * getStaffMonth)
-    let zp = Math.ceil(newZp / 10) * 10
-    
-    if (getNowDate === getStaffDate && getNowMonth === getStaffMonth) {
-      return `У ${f} сегодня праздник, его премия составляет ${zp}`
-    } else {
-      return `У ${f} сегодня обычный день, поэтому без премии`
-    }
-  };
+export const calculateCashback = (isCashback, sum, cb) => {};
 
-  const f = "Andrey Rumiantsev";
-  const s = "1995-11-09T08:13:51.376Z"
-  const t = "500"
+export const getHolidaysNotifications = (f, s) => {};
 
-  console.log(getHolidayPrizes(f, s, t) )
+export const getHolidayPrizes = (f, s, t) => {
+  let nowDate = new Date();
+  let getNowDate = nowDate.getDate();
+  let getNowMonth = nowDate.getMonth();
+  let staffDate = new Date(Date.parse(s));
+  let getStaffDate = staffDate.getDate();
+  let getStaffMonth = staffDate.getMonth();
 
- 
+  let newZp = (10 * t) / 100 + getStaffDate * getStaffMonth;
+  let zp = Math.ceil(newZp / 10) * 10;
+  console.log(zp);
+
+  if (getNowDate === getStaffDate && getNowMonth === getStaffMonth) {
+    return `У ${f} сегодня праздник, его премия составляет ${zp}`;
+  } else {
+    return `У ${f} сегодня обычный день, поэтому без премии`;
+  }
+};
